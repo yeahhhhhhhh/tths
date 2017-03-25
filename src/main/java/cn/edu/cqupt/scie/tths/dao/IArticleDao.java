e10404514e28ba6d694fc932a02a5ada4f176d42
Kevin <1284629130@qq.com>
2017-03-26 01:18:22 +0800

    DAO借口

---
+++ src/main/java/cn/edu/cqupt/scie/tths/dao/IArticleDao.java
@@ -0,0 +1,23 @@
+package cn.edu.cqupt.scie.tths.dao;
+
+import cn.edu.cqupt.scie.tths.model.ArticleModel;
+import cn.edu.cqupt.scie.tths.model.page.Page;
+
+import java.util.List;
+
+/**
+ * Created by Kevin on 2017/3/26.
+ */
+public interface IArticleDao {
+    int saveArticle (ArticleModel articleModel);
+
+    int updateArticle(ArticleModel articleModel);
+
+    int deleteArticle(int articleId);
+
+    ArticleModel getArticle (int id);
+
+    List<ArticleModel> getArticleList (int uid, int cid, int statue,String searchString, Page page);
+
+    int getArticleListCount (int uid, int cid, int statue,String searchString, Page page);
+}
